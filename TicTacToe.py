44bc79f7b9c802ea40c9b0971c441652513bcfa7
Samantha <sschneibel@gaming.tech>
2022-11-21 15:20:44 +0100

    update pseudo code

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,29 +1,34 @@
-#on admet une fonction random
+#DEBUT
+
+#on admet qu'une fonction random existe qui renvoie un chiffre aléatoire
 from random import randint
 
-#créer un tableau vierge
+#créer la surface du jeu
 tableGame = [['-','-','-'],['-','-','-'],['-','-','-']]
 
-#définir une fonction showtable
+#créer la fonction tableGame qui renvoie la surface du jeu
 def showTable(tableGame):
-    print("---------------")
-    for row in tableGame:
+    print("\n--------------")
+    for row in tableGame :
         for item in row:
-            print (item, end="  ")
+            print(item, end="  ")
         print()
-    print("---------------\n")
-    
-def caseFilled(board, x, y):
-    if board[x][y] == '-':
+    print("--------------\n")
+
+#créer la fonction caseFilled qui vérifie que le coup d'un joueur est valide
+def caseFilled(board, posx, posy):
+    if board[posx][posy] == '-':
         return False
     else:
         return True
 
+#créer une fonction qui renvoie si un joueur gagne
 def is_player_win(board, player):
     win = None
 
     n = len(board)
 
+    #on vérifie les lignes
     for i in range(n):
         win = True
         for j in range(n):
@@ -33,6 +38,7 @@ def is_player_win(board, player):
         if win:
             return win
 
+    #on vérifie les colonnes
     for i in range(n):
         win = True
         for j in range(n):
@@ -42,6 +48,7 @@ def is_player_win(board, player):
         if win:
             return win
 
+    #on vérifie les diagonales
     win = True
     for i in range(n):
         if board[i][i] != player:
@@ -59,6 +66,7 @@ def is_player_win(board, player):
         return win
     return False
 
+#on vérifie si la surface du jeu est pleine 
 def is_board_filled(board):
     for row in board:
         for item in row:
@@ -66,39 +74,70 @@ def is_board_filled(board):
                 return False
     return True
 
-def morpion():
-    global tableGame
-    playerOne = input("Joueur X, entrez votre pseudo : \n")
-    playerTwo = input("Joueur O, entrez votre pseudo : \n")
+#créer une variable startGame et lui associer le retour de l'exécution de la fonction input pour demander de jouer ou non
+startGame = input("Voulez-vous jouer ? oui ou non : \n")
+#si starGame est à oui
+if startGame == "oui":
+    #alors le jeu se lance
+    continuer = True
+    playerOne = input("Joueur X choisissez votre pseudo : \n")
+    playerTwo = input("Joueur O choisissez votre pseudo : \n")
+#sinon le jeu s'arrête
+elif startGame == "non":
+    print("\nTant pis, à une prochaine ! \n")
+    continuer = False
+#sinon redemander la question (2 chances)
+else:
+    startGame = input("Voulez-vous jouer ? oui ou non : \n")
+    if startGame == "oui":
+        continuer = True
+        playerOne = input("Joueur X choisissez votre pseudo : \n")
+        playerTwo = input("Joueur O choisissez votre pseudo : \n")
+    elif startGame == "non":
+        print("\nTant pis, à une prochaine ! \n")
+        continuer = False
+    else:
+        startGame = input("Voulez-vous jouer ? oui ou non : \n")
+        if startGame == "oui":
+            continuer = True
+            playerOne = input("Joueur X choisissez votre pseudo : \n")
+            playerTwo = input("Joueur O choisissez votre pseudo : \n")
+        elif startGame == "non":
+            print("\nTant pis, à une prochaine ! \n")
+            continuer = False
+
+#tant que la variable continuer est égale à vrai alors le jeu se lance
+while continuer == True :
+    
     playerOneShoot = "X"
     playerTwoShoot = "O"
     playerStart = randint(1, 2)
+    
     if playerStart == 1:
-        print(playerOne + ", commence")
         playerTurn = playerOne
         playerShoot = playerOneShoot
     else:
-        print(playerTwo + ", commence")
         playerTurn = playerTwo
         playerShoot = playerTwoShoot
+    
     correctShoot = False
-    playerWin = False
+    playerWin = False 
     
     showTable(tableGame)
     while playerWin == False:
         while correctShoot == False:
-            print("Tour de " + playerTurn + "\n")
-            choiceX = int(input("Ligne: (ligne 1 = 0, ligne 2 = 1 et ligne 3 = 2 \n)"))
-            choiceY = int(input("Colonne: (colonne 1 = 0, colonne 2 = 1 et colonne 3 = 2 \n)"))
+            print("Au Tour de " + playerTurn + "\n")
+            choiceX = int(input("Choisissez la ligne à modifier (ligne 1 = 0 / ligne 2 = 1 / ligne 3 = 2) : \n"))
+            choiceY = int(input("Choisissez la colonne à modifier (colonne 1 = 0 / colonne 2 = 1 / colonne 3 = 2) : \n"))
             if caseFilled(tableGame, choiceX, choiceY) != True:
                 tableGame[choiceX][choiceY] = playerShoot
-                correctShoot = True
+                correctShoot = True 
         showTable(tableGame)
 
         if is_player_win(tableGame, playerShoot):
-            print("Le joueur " + playerTurn + " à gagné la partie ! :)")
+            print("Le joueur " + playerTurn +  " à gagné la partie ! :) ")
             break
-
+        
         if is_board_filled(tableGame):
             print("Egalité ! :/")
             break
@@ -106,18 +145,41 @@ def morpion():
         if playerShoot == playerOneShoot:
             playerShoot = playerTwoShoot
             playerTurn = playerTwo
-        else:
+        else : 
             playerShoot = playerOneShoot
             playerTurn = playerOne
 
         correctShoot = False
     
-    otherGame = input("Voulez-vous rejouer ? oui ou non : ")
+    #créer une variable otherGame et lui associer le retour de l'exécution de la fonction input qui demande si les joueurs veulent refaire une partie
+    otherGame = input("Voulez-vous rejouer ? oui ou non : \n")
+    #si otherGame est égal à oui alors le jeu se relance
     if otherGame == "oui":
-        tableGame = [['-','-','-'],['-','-','-'],['-','-','-']]
         print("C'est reparti !")
-        morpion()
-    elif otherGame == "no":
+        tableGame = [['-','-','-'],['-','-','-'],['-','-','-']]
+        continuer = True
+    #si otherGame est égal à non alors le jeu s'arrête
+    elif otherGame == "non":
         print("A bientôt !")
+        continuer = False
+    #sinon redemander la question (2 chances)
+    else:
+        otherGame = input("Voulez-vous rejouer ? oui ou non : \n")
+        if otherGame == "oui":
+            print("C'est reparti !")
+            tableGame = [['-','-','-'],['-','-','-'],['-','-','-']]
+            continuer = True
+        elif otherGame == "non":
+            print("A bientôt !")
+            continuer = False
+        else:
+            otherGame = input("Voulez-vous rejouer ? oui ou non : \n")
+            if otherGame == "oui":
+                print("C'est reparti !")
+                tableGame = [['-','-','-'],['-','-','-'],['-','-','-']]
+                continuer = True
+            elif otherGame == "non":
+                print("A bientôt !")
+                continuer = False
 
-morpion()
+#FIN
--- pseudocode.py
+++ pseudocode.py
@@ -1,5 +1,170 @@
-#on admet une fonction random
+#DEBUT
 
-#créer un tableau vierge
+#on admet qu'une fonction random existe qui renvoie un chiffre aléatoire
+#on damet qu'une fonction input existe qui renvoie l'entrée de l'utilisateur
 
-#définir une fonction showTable
+#créer une liste tableJeu qui servira de table de jeu [['-','-','-'],['-','-','-'],['-','-','-']]
+
+#définir la fonction showTable qui renvoie en paramètre (tableJeu)
+    #alors afficher ("\n--------------")
+    #pour les lignes dans tableJeu
+        #pour les items dans la ligne
+            #afficher (item, end="  ")
+        #afficher ()
+    #afficher ("--------------\n")
+
+#définir la fonction caseFilled qui renvoie en paramètres (board, posx et posy)
+    #si board[posx][posy] est égal à '-'
+        #alors renvoyer faux
+    #sinon 
+        #alors renvoyer vrai
+
+#definir la fonction is_player_win qui renvoie en paramètres (board et player)
+    #créer la variable win égale à None
+
+    #créer la variable n égale à la longueur de board
+ 
+    #pour i allant de 0 à n-1
+        #win est égal à vrai
+        #pour j allant de 0 à n-1
+            #si board[i][j] différent de player
+                #alors win est égal à faux
+                #alors casser la boucle
+        #si win
+            #alors renvoyer win
+
+    
+    #pour i allant de 0 à n-1
+        #win est égal à vrai
+        #pour j allant de 0 à n-1
+            #si board[j][i] différent de player
+                #alors win est égal à faux
+                #alors casser la boucle
+        #si win
+            #alors renvoyer win
+
+    #win est égal à vrai
+    #pour i allant de 0 à n-1
+        #si board[i][i] est différent de player
+            #alors win est égal à faux
+            #alors casser la boucle
+    #si win
+        #alors renvoyer win
+    #renvoyer faux
+
+    #win est égal à vrai
+    #pour i allant de 0 à n-1
+        #si board[i][n - 1 - i] est différent de player
+            #alors win est égal à faux
+            #alors casser la boucle
+    #si win
+        #alors renvoyer win
+    #renvoyer faux
+
+ 
+#définir la fonction is_board_filled de paramètre (board)
+    #pour les lignes dans les board
+        #pour les items dans les lignes
+            #si item est égal à '-'
+                #alors renvoyer faux
+    #renvoyer vrai
+
+#créer une variable startGame et lui associer le retour de l'exécution de la fonction input pour demander ("Voulez-vous jouer ? oui ou non : ")
+    #si startGame est égal à oui
+        #alors créer une variable continuer égale à vrai
+        #alors créer une variable playerOne et lui associer le retour de l'exécution de la fonction input pour demander ("Joueur X choisissez votre pseudo : \n")
+        #alors créer une variable plauerTwo et lui associer le retour de l'exécution de la fonction input pour demander ("Joueur O choisissez votre pseudo : \n")
+    #si startGame est égal à non
+        #alors afficher ("\nTant pis, à une prochaine ! \n")
+        #alors continuer est faux
+    #sinon
+        #alors utiliser la variable startGame et lui associer le retour de l'exécution de la fonction input pour demander ("Voulez-vous jouer ? oui ou non : ")
+        #si startGame est égal à oui
+            #alors créer une variable continuer égale à vrai
+            #alors créer une variable playerOne et lui associer le retour de l'exécution de la fonction input pour demander ("Joueur X choisissez votre pseudo : \n")
+            #alors créer une variable plauerTwo et lui associer le retour de l'exécution de la fonction input pour demander ("Joueur O choisissez votre pseudo : \n")
+        #si startGame est égal à non
+            #alors afficher ("\nTant pis, à une prochaine ! \n")
+            #alors continuer est faux
+        #sinon
+            #alors utiliser la variable startGame et lui associer le retour de l'exécution de la fonction input pour demander ("Voulez-vous jouer ? oui ou non : ")
+            #si startGame est égal à oui
+                #alors créer une variable continuer égale à vrai
+                #alors créer une variable playerOne et lui associer le retour de l'exécution de la fonction input pour demander ("Joueur X choisissez votre pseudo : \n")
+                #alors créer une variable plauerTwo et lui associer le retour de l'exécution de la fonction input pour demander ("Joueur O choisissez votre pseudo : \n")
+            #si startGame est égal à non
+                #alors afficher ("\nTant pis, à une prochaine ! \n")
+                #alors continuer est faux
+
+#tant que la continuer est égal à vrai
+    
+    #créer une varaible playerOneShoot égale à X
+    #créer une variable playerTwoShoot égal à O
+    #créer une variable playStart qui renvoie un chiffre aléatoire entre 1 et 2
+
+    #si playerStart est égal à 1
+        #alors créer une variable playerTurn égale à playerOne
+        #alors créer une variable playerShoot égale à playerOneShoot
+    #sinon
+        #alors créer une variable playerTurn égale à playerTwo
+        #alors créer une variable playerShoot égale à playerTwoShoot
+
+    #créer une variable correctShoot égale à faux
+    #créer une variable playerWin égale à faux
+
+    #exécuter la fonction showTable avec en paramètre (tableJeu)
+    #tant que playerWin est égal à faux
+        #tant que correctShoot est égal à faux
+            #afficher ("Au Tour de " + playerTurn + "\n")
+            #créer une variable choiceX et lui associer le retour de l'exécution de la fonction input pour demander ("Choisissez la ligne à modifier (ligne 1 = 0 / ligne 2 = 1 / ligne 3 = 2) : \n")
+            #créer une variable choiceY et lui associer le retour de l'exécution de la fonction input pour demander ("Choisissez la colonne à modifier (colonne 1 = 0 / colonne 2 = 1 / colonne 3 = 2) : \n")
+            #si la fonction caseFilled avec en paramètres (tablegame, choiceX et choiceY) est différent de vrai
+                #alors tableGame[choiceX][choiceY] est égal à playerShoot
+                #alors correctShoot est vrai
+        #éxécuter la fonction showTable avec en paramètre (tableJeu)
+
+        #si la fonction is_board_win de paramètres (tableGame et playerShoot)
+            #alors afficher ("Le joueur " + playerTurn +  " à gagné la partie ! :) ")
+            #alors casser la boucle
+
+        #si la fonction is_board_filled de paramètre (tableGame)
+            #alors afficher ("Egalité ! :/")
+            #alors casser la boucle
+
+        #si playerShoot est égal à playerOneShoot
+            #alors playerShoot est égal à playerTwoShoot
+            #alors playerTurn est égal à playerTwo
+        #sinon
+            #alors playerShoot est égal à playerOneShoot
+            #alors playerTurn est égal à playerOne
+
+        #correctShoot est faux
+    
+    #créer une variable otherGame et lui associer le retour de l'exécution de la fonction input pour demander ("Voulez-vous rejouer ? oui ou non : \n")
+    #si otherGame est égal à oui
+        #alors afficher ("C'est reparti !")
+        #alors tableGame est égal à [['-','-','-'],['-','-','-'],['-','-','-']]
+        #alors continuer est vrai
+    #si otherGame est égal à non
+        #alors afficher ("A bientôt !")
+        #alors continuer est faux
+    #sinon
+        #utiliser otherGame et lui associer le retour de l'exécution de la fonction input pour demander ("Voulez-vous rejouer ? oui ou non : \n")
+        #si otherGame est égal à oui
+            #alors afficher ("C'est reparti !")
+            #alors tableGame est égal à [['-','-','-'],['-','-','-'],['-','-','-']]
+            #alors continuer est vrai
+        #si otherGame est égal à non
+            #alors afficher ("A bientôt !")
+            #alors continuer est faux
+        #sinon
+            #utiliser otherGame et lui associer le retour de l'exécution de la fonction input pour demander ("Voulez-vous rejouer ? oui ou non : \n")
+            #si otherGame est égal à oui
+                #alors afficher ("C'est reparti !")
+                #alors tableGame est égal à [['-','-','-'],['-','-','-'],['-','-','-']]
+                #alors continuer est vrai
+            #si otherGame est égal à non
+                #alors afficher ("A bientôt !")
+                #alors continuer est faux
+
+#FIN
